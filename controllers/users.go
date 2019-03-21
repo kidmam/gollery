d576ee1d5f68b4f6d37c75c66e482bc0def40ac3
Max Li <maxlivinci@gmail.com>
2019-03-21 08:19:20 +0100

    HTTP request methods and gorilla/mux

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/LIYINGZHEN/gollery/views"
@@ -8,7 +9,7 @@ import (
 
 func NewUsers() *Users {
 	return &Users{
-		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+		NewView: views.NewView("bootstrap", "views/users/new.html"),
 	}
 }
 
@@ -20,4 +21,8 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a temporary response.")
 }
