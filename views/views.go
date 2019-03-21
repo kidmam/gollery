ef131aeaef777534b81f60f3905fc3939bfae771
Max Li <maxlivinci@gmail.com>
2019-03-21 06:57:52 +0100

    cleaning up our code

--- views/views.go
+++ views/views.go
@@ -1,17 +1,30 @@
 package views
 
-import "text/template"
+import (
+	"path/filepath"
+	"text/template"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".html"
+)
 
 type View struct {
 	Template *template.Template
 	Layout   string
 }
 
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/footer.html",
-		"views/layouts/navbar.html",
-		"views/layouts/bootstrap.html")
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
