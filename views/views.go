05303bd6dc37f7401f9b3e721298cad88eeedd64
Max Li <maxlivinci@gmail.com>
2019-03-21 06:48:39 +0100

    adding a navigation bar

--- views/views.go
+++ views/views.go
@@ -10,6 +10,7 @@ type View struct {
 func NewView(layout string, files ...string) *View {
 	files = append(files,
 		"views/layouts/footer.html",
+		"views/layouts/navbar.html",
 		"views/layouts/bootstrap.html")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
