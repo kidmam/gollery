37cbf5c48838c0bcbe7228f1c11e78b1f8ac715f
Max Li <maxlivinci@gmail.com>
2019-03-22 15:48:56 +0100

    Using the users service in our users controller

--- controllers/users.go
+++ controllers/users.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/LIYINGZHEN/gollery/models"
 	"github.com/LIYINGZHEN/gollery/views"
 )
 
-func NewUsers() *Users {
+func NewUsers(us *models.UserService) *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "users/new"),
-	}
+		us:      us}
 }
 
 type Users struct {
 	NewView *views.View
+	us      *models.UserService
 }
 
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
@@ -34,7 +36,13 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, "Name is", form.Name)
-	fmt.Fprintln(w, "Email is", form.Email)
-	fmt.Fprintln(w, "Password is", form.Password)
+	user := models.User{
+		Name:  form.Name,
+		Email: form.Email,
+	}
+	if err := u.us.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, "User is", user)
 }
--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	us.AutoMigrate()
 
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	usersC := controllers.NewUsers(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
