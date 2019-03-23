4131ea81d7bbae453d3f2630ea25709ddb74aa65
Max Li <maxlivinci@gmail.com>
2019-03-23 06:59:23 +0100

    Adding remember token fields to our User type

--- models/user.go
+++ models/user.go
@@ -24,6 +24,8 @@ type User struct {
 	Email        string `gorm:"not null;unique_index"`
 	Password     string `gorm:"-"`
 	PasswordHash string `gorm:"not null"`
+	Remember     string `gorm:"-"`
+	RememberHash string `gorm:"not null;unique_index"`
 }
 
 // NewUserService creates the UserService.
