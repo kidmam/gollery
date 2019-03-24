a5913a3659ba94efded6abdb4097423ea5d6f40b
Max Li <maxlivinci@gmail.com>
2019-03-24 17:01:09 +0100

    Controller actions and views

--- main.go
+++ main.go
@@ -59,6 +59,10 @@ func main() {
 	r.Handle("/logout",
 		requireUserMw.ApplyFn(usersC.Logout)).
 		Methods("POST")
+	r.Handle("/forgot", usersC.ForgotPwView).Methods("GET")
+	r.HandleFunc("/forgot", usersC.InitiateReset).Methods("POST")
+	r.HandleFunc("/reset", usersC.ResetPw).Methods("GET")
+	r.HandleFunc("/reset", usersC.CompleteReset).Methods("POST")
 	r.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
 
 	// Gallery routes
