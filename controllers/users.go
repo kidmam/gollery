5567b92c175439b9843bef142da068803937d5da
Max Li <maxlivinci@gmail.com>
2019-03-24 16:54:11 +0100

    Persisting and prefilling form data

--- controllers/users.go
+++ controllers/users.go
@@ -33,7 +33,9 @@ type Users struct {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	u.NewView.Render(w, r, nil)
+	var form SignupForm
+	parseURLParams(r, &form)
+	u.NewView.Render(w, r, form)
 }
 
 type SignupForm struct {
@@ -49,6 +51,7 @@ type SignupForm struct {
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var vd views.Data
 	var form SignupForm
+	vd.Yield = &form
 	if err := parseForm(r, &form); err != nil {
 		vd.SetAlert(err)
 		u.NewView.Render(w, r, vd)
